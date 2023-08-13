b6406d0c8f4c7d3a48a48f686e8cb5f5d291f755
shubishukla <shubhangi.mudgal198@gmail.com>
2023-08-13 23:09:30 +0530

    feat(login): add password reset functionality to the app.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
     validates :name, presence: true, length: { maximum: 50 }
@@ -49,42 +49,59 @@ class User < ApplicationRecord
     end
     # Returns a session token to prevent session hijacking.
     # We reuse the remember digest for convenience.
-  def session_token
-    remember_digest || remember
-  end
-
- # Returns true if the given token matches the digest.
- def authenticated?(attribute, token)
-  digest = send("#{attribute}_digest")
-  return false if digest.nil?
-  BCrypt::Password.new(digest).is_password?(token)
- end
-
-  # Forgets a user.
-  def forget
-    update_attribute(:remember_digest, nil)
-  end
-  # Activates an account.
-  def activate
-    update_columns(activated: true, activated_at:Time.zone.now)
-
-  end
-
-  # Sends activation email.
-  def send_activation_email
-    UserMailer.account_activation(self).deliver_now
-  end
-
-  private
-
-    # Converts email to all lowercase.
-    def downcase_email
-      self.email = email.downcase
+    def session_token
+      remember_digest || remember
     end
 
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
+  # Returns true if the given token matches the digest.
+    def authenticated?(attribute, token)
+      digest = send("#{attribute}_digest")
+      return false if digest.nil?
+      BCrypt::Password.new(digest).is_password?(token)
     end
-end
+
+    # Forgets a user.
+    def forget
+      update_attribute(:remember_digest, nil)
+    end
+    # Activates an account.
+    def activate
+      update_columns(activated: true, activated_at:Time.zone.now)
+
+    end
+
+    # Sends activation email.
+    def send_activation_email
+      UserMailer.account_activation(self).deliver_now
+    end
+
+    #sets the password reset attributes.
+    def create_reset_digest
+      self.reset_token = User.new_token
+      update_columns(reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now)
+    end
+    #send password reset email.
+    def send_password_reset_email
+      UserMailer.password_reset(self).deliver_now
+    end
+
+    #Returns true if password reset has expired.
+    def password_reset_expired
+      reset_sent_at < 2.hours.ago
+    end
+
+
+
+    private
+
+      # Converts email to all lowercase.
+      def downcase_email
+        self.email = email.downcase
+      end
+
+      # Creates and assigns the activation token and digest.
+      def create_activation_digest
+        self.activation_token  = User.new_token
+        self.activation_digest = User.digest(activation_token)
+      end
+  end
